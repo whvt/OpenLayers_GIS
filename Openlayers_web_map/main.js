d2e8b700680e2075fc3cbe1783253db4b11a325c
Nikita Bulankov <xm987654321@gmail.com>
2020-06-02 18:21:18 +0300

    fix: change view settings

--- Openlayers_web_map/main.js
+++ Openlayers_web_map/main.js
@@ -2,20 +2,20 @@ window.onload = init;
 function init(){
   const map = new ol.Map({
     view: new ol.View({
-      center: [0,0],
-      zoom: 3,      
-      rotation: 0.5 
+      center: [3068140.6253183694, 7154008.0711093814],
+      zoom: 7,       
+      minZoom: 2,
+      rotation: 0 
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    target: 'js-map',
-    keyboardEventTarget: document
+    target: 'js-map'
   })
 
- /*  const popupContainerElement = document.getElementById('popup-coordinates');
+  const popupContainerElement = document.getElementById('popup-coordinates');
   const popup = new ol.Overlay({
     element: popupContainerElement,
     positioning: 'top-right'
@@ -23,31 +23,17 @@ function init(){
 
   map.addOverlay(popup);
 
-  map.on('click', function(e){
+  map.on('dblclick', function(e){
     const clickedCoordinate = e.coordinate;
     popup.setPosition(undefined);
     popup.setPosition(clickedCoordinate);
     popupContainerElement.innerHTML = clickedCoordinate;
-  }) */
-  
-  // DragRotate Interaction
-  const dragRotateInteraction = new ol.interaction.DragRotate({
+  }) 
+// DragRotate Interaction
+const dragRotateInteraction = new ol.interaction.DragRotate({
     condition: ol.events.condition.altKeyOnly
   })
+  map.addInteraction(dragRotateInteraction) 
 
-  map.addInteraction(dragRotateInteraction)
-
-/*   const drawInteraction = new ol.interaction.Draw({
-    type: 'Polygon',
-    freehand: true
-  })
-  map.addInteraction(drawInteraction);
-
-  drawInteraction.on('drawend', function(e){
-    let parser = new ol.format.GeoJSON();
-    let drawnFeatures = parser.writeFeatures([e.feature]);
-    console.log(drawnFeatures);
-  }) */
 }
-
 
