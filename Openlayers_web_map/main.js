8199433308a7874afec6dae1e817f7db040e7d99
Nikita Bulankov <xm987654321@gmail.com>
2020-06-02 19:41:18 +0300

    feat: add advanced interactions(zoom level, fullscreen mode, minimap, minimap collapse ability); add adaptive scale level

--- Openlayers_web_map/main.js
+++ Openlayers_web_map/main.js
@@ -1,19 +1,46 @@
 window.onload = init;
+
 function init(){
+  // Controls
+  const fullScreenControl = new ol.control.FullScreen();
+  const mousePositionControl = new ol.control.MousePosition();
+  const overViewMapControl = new ol.control.OverviewMap({
+    collapsed: false,
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()      
+      })
+    ]
+  });
+  const scaleLineControl = new ol.control.ScaleLine();
+  const zoomSliderControl = new ol.control.ZoomSlider();
+  const zoomToExtentControl = new ol.control.ZoomToExtent();
+
   const map = new ol.Map({
     view: new ol.View({
       center: [3068140.6253183694, 7154008.0711093814],
-      zoom: 7,       
-      minZoom: 2,
-      rotation: 0 
+      zoom: 6, 
+      maxZoom: 10,
+      minZoom: 1,
+      rotation: 0
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    target: 'js-map'
+    target: 'js-map',
+    keyboardEventTarget: document,
+    controls: ol.control.defaults().extend([
+      fullScreenControl,
+      mousePositionControl,
+      overViewMapControl,
+      scaleLineControl,
+      zoomSliderControl,
+      zoomToExtentControl
+    ])
   })
+  
 
   const popupContainerElement = document.getElementById('popup-coordinates');
   const popup = new ol.Overlay({
@@ -28,12 +55,26 @@ function init(){
     popup.setPosition(undefined);
     popup.setPosition(clickedCoordinate);
     popupContainerElement.innerHTML = clickedCoordinate;
-  }) 
-// DragRotate Interaction
-const dragRotateInteraction = new ol.interaction.DragRotate({
+  })
+  
+  // DragRotate Interaction
+  const dragRotateInteraction = new ol.interaction.DragRotate({
     condition: ol.events.condition.altKeyOnly
   })
-  map.addInteraction(dragRotateInteraction) 
 
+  map.addInteraction(dragRotateInteraction)
+
+  /* const drawInteraction = new ol.interaction.Draw({
+    type: 'Polygon',
+    freehand: true
+  })
+  map.addInteraction(drawInteraction);
+
+  drawInteraction.on('drawend', function(e){
+    let parser = new ol.format.GeoJSON();
+    let drawnFeatures = parser.writeFeatures([e.feature]);
+    console.log(drawnFeatures);
+  }) */
 }
+
 
