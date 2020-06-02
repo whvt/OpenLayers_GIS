16890d3842fc607ad58b82a8c324d35c778dfebb
Nikita Bulankov <xm987654321@gmail.com>
2020-06-02 17:53:45 +0300

    feat: add drag and rotate interactions, add draw feature (commented)

--- Openlayers_web_map/main.js
+++ Openlayers_web_map/main.js
@@ -1,17 +1,53 @@
 window.onload = init;
-
 function init(){
   const map = new ol.Map({
     view: new ol.View({
-      center: [0, 0],
-      zoom: 2
+      center: [0,0],
+      zoom: 3,      
+      rotation: 0.5 
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
       })
     ],
-    target: 'js-map'
+    target: 'js-map',
+    keyboardEventTarget: document
   })
+
+ /*  const popupContainerElement = document.getElementById('popup-coordinates');
+  const popup = new ol.Overlay({
+    element: popupContainerElement,
+    positioning: 'top-right'
+  })
+
+  map.addOverlay(popup);
+
+  map.on('click', function(e){
+    const clickedCoordinate = e.coordinate;
+    popup.setPosition(undefined);
+    popup.setPosition(clickedCoordinate);
+    popupContainerElement.innerHTML = clickedCoordinate;
+  }) */
+  
+  // DragRotate Interaction
+  const dragRotateInteraction = new ol.interaction.DragRotate({
+    condition: ol.events.condition.altKeyOnly
+  })
+
+  map.addInteraction(dragRotateInteraction)
+
+/*   const drawInteraction = new ol.interaction.Draw({
+    type: 'Polygon',
+    freehand: true
+  })
+  map.addInteraction(drawInteraction);
+
+  drawInteraction.on('drawend', function(e){
+    let parser = new ol.format.GeoJSON();
+    let drawnFeatures = parser.writeFeatures([e.feature]);
+    console.log(drawnFeatures);
+  }) */
 }
+
 
